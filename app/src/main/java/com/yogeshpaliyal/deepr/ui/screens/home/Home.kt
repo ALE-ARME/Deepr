8699c47ebc5b49c879adc5dec34940faa2e184e4
ALE-ARME <alessioarm69@gmail.com>
2026-08-26 15:43:45 +0200

    chore: fix import ordering in Home.kt

--- app/src/main/java/com/yogeshpaliyal/deepr/ui/screens/home/Home.kt
+++ app/src/main/java/com/yogeshpaliyal/deepr/ui/screens/home/Home.kt
@@ -82,13 +82,13 @@ import androidx.compose.material3.rememberSearchBarState
 import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
